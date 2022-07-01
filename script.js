a591c91213d81007be2d7c401743ed0bbe6805cb
Matheus Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2022-07-01 11:39:55 -0300

    error ao mostrar valor

---
+++ script.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('#send')
+
+btn.addEventListener("click", function (e) {
+
+    e.preventDefault();
+
+    const name = document.querySelector('#tarefa');
+    const value = name.value;
+    value.innerHTML += `${value}`
+
+});
