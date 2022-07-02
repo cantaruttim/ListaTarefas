741ef19bc1c056a0803c9891b64c1f14a5c7a3bb
Matheus Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2022-07-02 08:41:37 -0300

    ajust section para mostrar tarefas

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ btn.addEventListener("click", function (e) {
     const name = document.querySelector('#tarefa');
     const value = name.value;
 
-    const div = document.querySelector('.bloco'); //seleciona a div pai
+    const div = document.querySelector('.bloco_res'); //seleciona a div pai
     const saida = document.createElement('li'); // cria a li
     div.appendChild(saida); // define a li como filha da div bloco
     saida.innerHTML = `${value}`; // passa o valor do input para o li
